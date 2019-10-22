7a3b8551edbf910abab14830a9ef78775c8f3368
Pythondeveloper6 <pythondeveloper6@gmail.com>
2019-10-22 10:40:42 +0200

    add new category for organize and more extensions

--- Organise-Directory-Files.py
+++ Organise-Directory-Files.py
@@ -4,17 +4,17 @@ import shutil
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 try:
-    print("Organising your files...")
+    print("Organising your files intro [ images - music - video -executable - archive - torrent - document - code ]")
     for filename in os.listdir(dir_path):
-        # Check if files are images
+        # Check if files are images and you can add more extentions 
         if filename.lower().endswith((".png", ".jpg", ".jpeg", ".gif", ".bmp", ".pbm", ".pnm")):
-            # If images folder doesnt exist then create
+            # If images folder doesnt exist then create new folder
             if not os.path.exists("images"):
                 os.makedirs("images")
             shutil.copy2(filename, "images")
             os.remove(filename)
 
-        # Check if files are music
+        # Check if files are music and you can add more extentions
         if filename.lower().endswith((".wav", ".mp3", ".flac", ".3gp", ".aa", ".aax", ".aiff", ".raw")):
             # If music folder doesnt exist then create
             if not os.path.exists("music"):
@@ -22,24 +22,59 @@ try:
             shutil.copy2(filename, "music")
             os.remove(filename)
 
+        # Check if files are videos and you can add more extentions
+        if filename.lower().endswith((".webm", ".mp4")):
+            # If music folder doesnt exist then create
+            if not os.path.exists("music"):
+                os.makedirs("music")
+            shutil.copy2(filename, "music")
+            os.remove(filename)
+
         # Check if files are executables
-        if filename.lower().endswith((".exe", ".tar", ".deb")):
+        if filename.lower().endswith((".exe", ".msi", ".deb")):
             # If executables folder doesnt exist then create
             if not os.path.exists("executables"):
                 os.makedirs("executables")
             shutil.copy2(filename, "executables")
             os.remove(filename)
 
+        # Check if files are archive files
+        if filename.lower().endswith((".rar", ".tar" , ".zip" , ".gz")):
+            # If executables folder doesnt exist then create
+            if not os.path.exists("archives"):
+                os.makedirs("archives")
+            shutil.copy2(filename, "archives")
+            os.remove(filename)
+
+
+        # Check if files are torrent files
+        if filename.lower().endswith((".torrent",)):
+            # If executables folder doesnt exist then create
+            if not os.path.exists("torrent"):
+                os.makedirs("torrent")
+            shutil.copy2(filename, "torrent")
+            os.remove(filename)
+
         # Check if files are documents
-        if filename.lower().endswith((".txt", ".pdf", ".docx")):
+        if filename.lower().endswith((".txt", ".pdf", ".docx" , "doc")):
             # If executables folder doesnt exist then create
             if not os.path.exists("documents"):
                 os.makedirs("documents")
             shutil.copy2(filename, "documents")
             os.remove(filename)
 
+
+        # Check if files are code files
+        if filename.lower().endswith((".py", ".php", ".html" , ".css" , ".js")):
+            # If executables folder doesnt exist then create
+            if not os.path.exists("code"):
+                os.makedirs("code")
+            shutil.copy2(filename, "code")
+            os.remove(filename)
+
+
 except OSError:
-    print("Error")
+    print("Error happened ...... try again")
 finally:
     # When script is finished clear screen and display message
     os.system("cls" if os.name == "nt" else "clear")
